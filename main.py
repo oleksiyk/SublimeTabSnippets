a5c94553ffd5b475c2603399690c4f998d44efc4
Oleksiy Krivoshey <oleksiy@luckyteam.co.uk>
2016-04-04 16:53:25 +0300

    Just insert TAB when no matching snippet found

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from threading import Timer
 class HideAutocompleteAndInsertSnippetCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		self.view.run_command("hide_auto_complete")
-		t = Timer(0.01, self.view.run_command, ["insert_best_completion"])
+		t = Timer(0.01, self.view.run_command, ["insert_best_completion", {"default": "\t", "exact": True}])
 		t.start()
